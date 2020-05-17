3d14d5dca6185f61e23913d2cd8e66dff76aa273
OoDone <48130949+OoDone@users.noreply.github.com>
2020-05-16 22:34:20 -0400

    Update get.js

--- get.js
+++ get.js
@@ -1,17 +1,13 @@
 const got = require('got');
-module.exports.api = function (port, callback) {
-  got("https://api.mcsrvstat.us/2/37.59.209.192:" + port, { json: false }).then(response => {
+module.exports.api = function (username) {
+  got('https://api.hypixel.net/player?key=e4e148ec-6bfc-4f8c-b313-453ca5571472&name=' + username, { json: false }).then(response => {
     global.parsedjson = JSON.parse(response.body);
-    //console.log(port);
-    global.port = port;
-    global.isonline = parsedjson['online'];
-    console.log(global.isonline + " " + parsedjson['port']);
-    //console.log(response.body);
-    //global.bedwarslevel = "Level: " + parsedjson['player']['achievements']['bedwars_level'];
-    callback(null, global.isonline);
-    //callback(null, global.fk);
+    console.log(username);
+    global.username = username;
+    global.losses = "gamesplayed: " + parsedjson['player']['stats']['Bedwars']['Games_played_bedwars_1'];
+    console.log(global.losses);
   }).catch(error => {
-    console.log("error: "+error.response);
+    console.log("error "+error.response);
   });
   //callback(return global.bedwarslevel);
 }
